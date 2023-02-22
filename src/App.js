652515c9080c1f29a22785479d6af1e706910b9e
Saad <saadabid5236@gmail.com>
2023-02-22 22:29:33 +0000

    basic game

--- src/App.js
+++ src/App.js
@@ -1,23 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [player, setPlayer] = useState("X");
+  const [board, setBoard] = useState(Array(9).fill(""));
+  const [winner, setWinner] = useState(null);
+
+  const checkWinner = (squares) => {
+    const possibleWins = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let i = 0; i < possibleWins.length; i++) {
+      const [a, b, c] = possibleWins[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+
+    return null;
+  };
+
+  const handleSquareClick = (index) => {
+    if (winner || board[index]) {
+      return;
+    }
+
+    const newBoard = [...board];
+    newBoard[index] = player;
+    setBoard(newBoard);
+
+    const newWinner = checkWinner(newBoard);
+    if (newWinner) {
+      setWinner(newWinner);
+    } else {
+      setPlayer(player === "X" ? "O" : "X");
+    }
+  };
+
+  const renderSquare = (index) => {
+    return (
+      <div className="square" onClick={() => handleSquareClick(index)}>
+        {board[index]}
+      </div>
+    );
+  };
+
+  const resetGame = () => {
+    setPlayer("X");
+    setBoard(Array(9).fill(""));
+    setWinner(null);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="board">
+        <div className="row">
+          {renderSquare(0)}
+          {renderSquare(1)}
+          {renderSquare(2)}
+        </div>
+        <div className="row">
+          {renderSquare(3)}
+          {renderSquare(4)}
+          {renderSquare(5)}
+        </div>
+        <div className="row">
+          {renderSquare(6)}
+          {renderSquare(7)}
+          {renderSquare(8)}
+        </div>
+      </div>
+      {winner && <div className="winner">Winner: {winner}</div>}
+      <button className="reset-button" onClick={resetGame}>
+        Reset Game
+      </button>
     </div>
   );
 }
